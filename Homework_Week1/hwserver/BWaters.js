4c050a037a8df7ec04acd67faf9591a818ac6e2e
bjwaters <32462984+bjwaters@users.noreply.github.com>
2019-01-14 13:44:53 -0500

    Add files via upload

---
+++ Homework_Week1/hwserver/BWaters.js
@@ -0,0 +1,65 @@
+//Error check to get todo to todo.json
+//Get the guithub working
+//get the read-todo to read-todo.html working
+
+var http = require('http');
+var url = require('url');
+var fileSystem = require('fs');
+
+
+http.createServer(function (request, response) {
+    
+    var pathName = url.parse(request.url).pathname;
+    var fileName = pathName.substr(1); /* lets remove the "/" from the name */
+
+    console.log(request.url)
+    console.log(".")
+
+    //This one does't work
+    if(fileName == "todo")
+    {
+        console.log("Filename is : " + fileName)
+        fileName ="todo.json"
+    }
+    else if(fileName == "read-todo")
+    {
+        fileName = "read-todo.html"
+    }
+    //This one
+    if(fileName == "todo.json")
+    {
+        console.log("Filename is : " + fileName)
+    }
+    if(fileName == "read-todo.html")
+    {
+        console.log("Filename is : " + fileName)
+    }
+
+    //This changes the content type based on the fileName
+    var contentType = "text/html"
+    if(fileName == "todo.json")
+    {
+        contentType = "application/json"
+    }
+
+    fileSystem.readFile(fileName , callback);
+
+    function callback(err, data) {
+        if (err) {
+            console.error(err);
+            response.writeHead(301, {'Location': "http://" + request.headers['host'] + '/index.html' });
+            console.log("Bad fileName was: " + fileName)
+        }
+        else {
+            response.writeHead(200, {'Content-Type': contentType})
+            response.write(data.toString())
+        }
+        
+        /* the response is complete */
+        response.end();
+    }
+   
+}).listen(3000);
+
+// Console will print the message
+console.log('Server running at http://localhost:3000/index.html');
